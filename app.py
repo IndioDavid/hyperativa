9c41239451e9cee7b81395f516cafc978016aa2a
David Fagundes <david.fagundes@sensedata.com.br>
2024-03-15 11:27:20 -0300

    :alien: add JWT_ALGORITHM

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ __CRED_OBJ__ = Credentials()
 class APP():
     def __init__(self):
         self.__app__ = Flask(__name__)
-        self.__app__.config['JWT_SECRET_KEY'] = "eyJhbGciOiJIUzI1NiJ9.ew0KICAic3ViIjogIjEyMzQ1Njc4OTAiLA0KICAibmFtZSI6ICJBbmlzaCBOYXRoIiwNCiAgImlhdCI6IDE1MTYyMzkwMjINCn0.OApQugx0HxMcTT-rSc-D_t_p399edsu4Bz8FRTSM0qE"
+        self.__app__.config['JWT_SECRET_KEY'] = __CRED_OBJ__.__SECRET__
         self.__app__.config["JWT_ALGORITHM"] = "HS256"
         self.__app__.register_blueprint(
             __blt__,
